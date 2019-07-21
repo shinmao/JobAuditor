f6ad435ffeddd21e6173c71eb2a13255ac1d6963
shinmao <shinmaox2016@gmail.com>
2019-07-20 20:53:05 -0700

    complete the function of listing agents/daemon

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Mojave auditor
+Important!! Plz run with `sudo python3 checker.py`!!.
 
 ## Requirement
+1. Your python version need to be at least 3.
+2. You need to install pandas module for python, I use it to export the reports.
 
-## Attention
-1. This auditor is best for Mojave.
+## Important
+1. Should run with sudo, or you would lost many information.
+2. This auditor is best for Mojave.
--- launchd.py
+++ launchd.py
@@ -2,71 +2,42 @@ import os
 import pandas as pd
 import numpy as np
 import re
-import xml.etree.ElementTree as ET
+import plistlib
 
 def convert2df(result):
-    col = ["Path", "Label", "Daemon/Agent", "Program", "Argument", "runtime", "maxCPU", "UserName", "GroupName", "Permission(8)"]
+    col = ["Path", "Label", "Daemon/Agent", "Program", "Argument", "maxCPU", "UserName", "GroupName", "Permission(8)"]
     df = pd.DataFrame(np.array(result), columns=col)
     df.to_csv(r"./report/job.csv")
 
 def complete_path(path, parentdir):
     return parentdir + "/" + path
 
-def parsexml(path):
-    label, program, argument, runtime, maxCPU, username, groupname, permission = "x", "x", "x", "x", "x", "x", "x", "x"
-    try:
-        tree = ET.parse(path)
-        root = tree.getroot()
-        time = ["RunAtLoad", 
-            "StartInterval", 
-            "StartCalendarInterval", 
-            "StartOnMount", 
-            "WatchPaths", 
-            "QueueDirectories", 
-            "KeepAlive"]
-        name = ["UserName",
-            "GroupName"]
-        for i in range(len(root[0])):
-            if root[0][i].text == "Label":
-                label = root[0][i+1].text
-            elif root[0][i].text == "Program":
-                program = root[0][i+1].text
-            elif root[0][i].text == "ProgramArguments":
-                program = root[0][i+1][0].text
-                argument = ""
-                j = 1
-                while j < len(root[0][i+1]):
-                    argument += root[0][i+1][j].text
-                    j += 1
-            elif root[0][i].text in time:
-                runtime = ""
-                runtime += root[0][i].text
-                runtime += "/"
-            elif root[0][i].text == "CPU":
-                maxCPU = root[0][i+1].text
-            elif root[0][i].text == "UserName":
-                username = root[0][i+1].text
-            elif root[0][i].text == "GroupName":
-                groupname = root[0][i+1].text
-            elif root[0][i].text == "Umask":
-                b10 = int(root[0][i+1].text)
-                permission = str(oct(b10))
-    except:
-        print("some invalid xml")
-    return label, program, argument, runtime, maxCPU, username, groupname, permission
+def parsePlist(path):
+    # label, program, argument, maxCPU, username, groupname, permission
+    attrib = ["x", "x", "x", "x", "x", "x", "x"]
+    key = ["Label", "Program", "ProgramArguments", "CPU", "UserName", "GroupName", "Umask"]
+    with open(path, 'rb') as f:
+        l = plistlib.load(f)
+        for i in range(len(key)):
+            try:
+                k = key[i]
+                attrib[i] = l[k]
+            except:
+                pass
+    f.close()
+    return attrib
 
-def appendProfile(path, job_kind, xmldata):
+def appendProfile(path, job_kind, data):
     profile = []
     profile.append(path)
-    profile.append(xmldata[0])
+    profile.append(data[0])
     profile.append(job_kind)
-    profile.append(xmldata[1])
-    profile.append(xmldata[2])
-    profile.append(xmldata[3])
-    profile.append(xmldata[4])
-    profile.append(xmldata[5])
-    profile.append(xmldata[6])
-    profile.append(xmldata[7])
+    profile.append(data[1])
+    profile.append(data[2])
+    profile.append(data[3])
+    profile.append(data[4])
+    profile.append(data[5])
+    profile.append(data[6])
     return profile
     
 def listjob():
@@ -77,8 +48,8 @@ def listjob():
     for i in range(len(usr_agent)):
         path = complete_path(usr_agent[i], usr_agent_path)
         job_kind = "agent"
-        xmldata = parsexml(path)
-        profile1 = appendProfile(path, job_kind, xmldata)
+        data = parsePlist(path)
+        profile1 = appendProfile(path, job_kind, data)
         service.append(profile1)
     # agent
     glob_agent_path = "/Library/LaunchAgents"
@@ -86,8 +57,8 @@ def listjob():
     for i in range(len(glob_agent)):
         path = complete_path(glob_agent[i], glob_agent_path)
         job_kind = "agent"
-        xmldata = parsexml(path)
-        profile2 = appendProfile(path, job_kind, xmldata)
+        data = parsePlist(path)
+        profile2 = appendProfile(path, job_kind, data)
         service.append(profile2)
     # daemon
     glob_daemon_path = "/Library/LaunchDaemons"
@@ -95,8 +66,8 @@ def listjob():
     for i in range(len(glob_daemon)):
         path = complete_path(glob_daemon[i], glob_daemon_path)
         job_kind = "daemon"
-        xmldata = parsexml(path)
-        profile3 = appendProfile(path, job_kind, xmldata)
+        data = parsePlist(path)
+        profile3 = appendProfile(path, job_kind, data)
         service.append(profile3)
     # agent
     sys_agent_path = "/System/Library/LaunchAgents"
@@ -104,8 +75,8 @@ def listjob():
     for i in range(len(sys_agent)):
         path = complete_path(sys_agent[i], sys_agent_path)
         job_kind = "agent"
-        xmldata = parsexml(path)
-        profile4 = appendProfile(path, job_kind, xmldata)
+        data = parsePlist(path)
+        profile4 = appendProfile(path, job_kind, data)
         service.append(profile4)
     # daemon
     sys_daemon_path = "/System/Library/LaunchDaemons"
@@ -113,7 +84,7 @@ def listjob():
     for i in range(len(sys_daemon)):
         path = complete_path(sys_daemon[i], sys_daemon_path)
         job_kind = "daemon"
-        xmldata = parsexml(path)
-        profile5 = appendProfile(path, job_kind, xmldata)
+        data = parsePlist(path)
+        profile5 = appendProfile(path, job_kind, data)
         service.append(profile5)
     convert2df(service)
